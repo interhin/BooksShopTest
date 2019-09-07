a07200fe1fb5cf514edffefaaa627adffedf039a
interhin <diman7277@gmail.com>
2019-09-07 20:04:58 +0700

    Updates

---
+++ precache-manifest.e55cd8615082c92fc98a4d6403c617b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eb9f2daeec28edf99dd",
+    "url": "/BooksShopTest/static/js/runtime~main.8eb9f2da.js"
+  },
+  {
+    "revision": "1d4c8d7bea5ce70d5e49",
+    "url": "/BooksShopTest/static/js/main.1d4c8d7b.chunk.js"
+  },
+  {
+    "revision": "14682c0656e4d5cd9763",
+    "url": "/BooksShopTest/static/js/1.14682c06.chunk.js"
+  },
+  {
+    "revision": "1d4c8d7bea5ce70d5e49",
+    "url": "/BooksShopTest/static/css/main.b19db910.chunk.css"
+  },
+  {
+    "revision": "11cd99befd1a32d2f7c0f350722a95ad",
+    "url": "/BooksShopTest/index.html"
+  }
+];
